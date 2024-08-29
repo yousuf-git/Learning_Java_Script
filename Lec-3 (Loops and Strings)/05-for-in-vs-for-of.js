57a47f6c3f3e6938bf5b1e7816e3f8c5f491afcd
yousuf-git <hackerbest052@gmail.com>
2024-08-29 19:40:19 +0500

    minor detail

--- Lec-3 (Loops and Strings)/05-for-in-vs-for-of.js
+++ Lec-3 (Loops and Strings)/05-for-in-vs-for-of.js
@@ -38,7 +38,8 @@ const peoples = [
     },
 ];
 
-// I have a peoples array, I want to iterate over its indices where I have Objects which have keys of different types. If any of the keys inside those objects is itslef another object, I want to iterate over the keys of that object and print their values on console
+// I have a peoples array, I want to iterate over its indices where I have Objects which have keys of different types. 
+// If any of the keys inside those objects is itslef another object, I want to iterate over the keys of that object and print their values on console
 
 /*
 
@@ -52,7 +53,8 @@ peoples
         |       |--- brother
         |--- age        
 
-At index 0 of peole we have an object which has 3 keys from which family itself has an object as value. So I want to print out those keys inside family and their values -> mother father brother
+At index 0 of peole we have an object which has 3 keys from which family itself has an object as value. 
+So I want to print out those keys inside family and their values -> mother father brother
 
  */
 
@@ -61,7 +63,7 @@ for (const obj of peoples) {
     // To Iterate over keys in object -> for-in loop
     for (const key in obj) {
         // If value of that key is an instance of Object then iterate over keys of that key
-        if (obj[key] instanceof Object) {
+        if (obj[key] instanceof Object) {           // obj[key] -> value stored in that key
             for (const innerKey in obj[key]) {
                 console.log(innerKey, " : ", obj[key][innerKey])
             }
